fd7906c7c099a7c98f281a6a49c47e97da928b3b
github username <github email address>
2021-05-29 20:58:48 -0400

    Got my Components to display.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,8 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  render(){
+    return <div>Hello</div>
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,8 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  render(){
+    return <div>World</div>
+  }
+}
